cedbf5812021f6afe79bbfb7c839db05c1881092
suzuuuuu09 <bsamu5788@gmail.com>
2024-07-25 21:26:18 +0900

    feat: モード選択機能追加

---
+++ README.md
@@ -0,0 +1,19 @@
+## 実行
+
+### お手軽モード(不安定)
+
+```
+python macro.py e
+```
+
+### お勧めモード
+
+```
+python macro.py n
+```
+
+### 高級モード
+
+```
+python macro.py h
+```
--- auto_type.py
+++ auto_type.py
@@ -5,13 +5,19 @@ import keyboard
 import cv2
 from time import sleep
 
-def typing():
+def typing(mode: str):
     location = image.find_image_on_screen("img/menu.png")
     x, y, scale = location[0], location[1], location[4]
-    print(x, y)
     for i in range(360):
         path = "img/text.png"
-        pg.screenshot(path, region = ((x + 77 * scale), y - 155 * scale, 345 * scale, 30 * scale))
+        r, w = 0, 0
+        if mode == "e":
+            r, w = x + 179 * scale, 150 * scale
+        elif mode == "n":
+            r, w = x + 128 * scale, 250 * scale
+        elif mode == "h":
+            r, w = x + 77 * scale, 345 * scale
+        pg.screenshot(path, region = (r, y - 155 * scale, w, 30 * scale))
         text = image.transcription(path)
         pg.typewrite(text)
         print(f"{text}", end="")
